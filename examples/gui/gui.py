247250724ed35fc4b06ebba53240e427e20284e5
Ian Ward <ian@excess.org>
2012-12-01 14:50:21 -0500

    rearrange buttons, add more adjustment steps

--- examples/gui/gui.py
+++ examples/gui/gui.py
@@ -16,18 +16,30 @@ class MainPanel(QtGui.QWidget):
         grid.setSpacing(10)
         grid.addWidget(self.screen, 0, 0, 8, 1)
         grid.setColumnMinimumWidth(0, 400)
+
+        y = 0
         antenna = QtGui.QComboBox(self)
         antenna.addItem("Antenna 1")
         antenna.addItem("Antenna 2")
         antenna.setEnabled(False)
-        grid.addWidget(antenna, 0, 1, 1, 2)
+        grid.addWidget(antenna, y, 1, 1, 2)
         bpf = QtGui.QComboBox(self)
         bpf.addItem("BPF On")
         bpf.addItem("BPF Off")
         bpf.setEnabled(False)
-        grid.addWidget(bpf, 0, 3, 1, 2)
+        grid.addWidget(bpf, y, 3, 1, 2)
 
-        grid.addWidget(QtGui.QLabel('Center Freq'), 1, 1, 1, 1)
+        y += 1
+        gain = QtGui.QComboBox(self)
+        gain.addItem("RF Gain: High")
+        gain.addItem("RF Gain: Low")
+        def new_gain():
+            self.update_gain(gain.currentText().split()[-1].lower())
+        gain.currentIndexChanged.connect(new_gain)
+        grid.addWidget(gain, y, 1, 1, 2)
+
+        y += 1
+        grid.addWidget(QtGui.QLabel('Center Freq'), y, 1, 1, 1)
         freq = QtGui.QLineEdit("")
         def read_freq():
             freq.setText("%0.1f" % self.get_freq_mhz())
@@ -39,41 +51,32 @@ class MainPanel(QtGui.QWidget):
                 return
             self.set_freq_mhz(f)
         freq.textEdited.connect(write_freq)
-        grid.addWidget(freq, 1, 2, 1, 2)
-        grid.addWidget(QtGui.QLabel('MHz'), 1, 4, 1, 1)
+        grid.addWidget(freq, y, 2, 1, 2)
+        grid.addWidget(QtGui.QLabel('MHz'), y, 4, 1, 1)
 
-        freq_plus = QtGui.QPushButton('+')
-        grid.addWidget(freq_plus, 2, 1, 1, 1)
+        y += 1
         steps = QtGui.QComboBox(self)
+        steps.addItem("Adjust: 1 MHz")
+        steps.addItem("Adjust: 2.5 MHz")
         steps.addItem("Adjust: 10 MHz")
+        steps.addItem("Adjust: 25 MHz")
         steps.addItem("Adjust: 100 MHz")
+        steps.setCurrentIndex(2)
         def freq_step(factor):
             try:
                 f = float(freq.text())
             except ValueError:
                 return read_freq()
-            delta = int(steps.currentText().split()[1]) * factor
+            delta = float(steps.currentText().split()[1]) * factor
             freq.setText("%0.1f" % (f + delta))
             write_freq()
-        grid.addWidget(steps, 2, 2, 1, 2)
+        grid.addWidget(steps, y, 2, 1, 2)
         freq_minus = QtGui.QPushButton('-')
-        freq_plus.clicked.connect(lambda: freq_step(1))
         freq_minus.clicked.connect(lambda: freq_step(-1))
-        grid.addWidget(freq_minus, 2, 4, 1, 1)
-
-        gain_plus = QtGui.QPushButton('+')
-        gain_plus.setEnabled(False)
-        grid.addWidget(gain_plus, 3, 1, 1, 1)
-        gain = QtGui.QComboBox(self)
-        gain.addItem("RF Gain: High")
-        gain.addItem("RF Gain: Low")
-        def new_gain():
-            self.update_gain(gain.currentText().split()[-1].lower())
-        gain.currentIndexChanged.connect(new_gain)
-        grid.addWidget(gain, 3, 2, 1, 2)
-        gain_minus = QtGui.QPushButton('-')
-        gain_minus.setEnabled(False)
-        grid.addWidget(gain_minus, 3, 4, 1, 1)
+        grid.addWidget(freq_minus, y, 1, 1, 1)
+        freq_plus = QtGui.QPushButton('+')
+        freq_plus.clicked.connect(lambda: freq_step(1))
+        grid.addWidget(freq_plus, y, 4, 1, 1)
 
         self.setLayout(grid)
         self.setWindowTitle('ThinkRF WSA4000')
