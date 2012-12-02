1315d77815783da095fe0467de855ae3fc53f3dc
Ian Ward <ian@excess.org>
2012-12-02 14:46:21 -0500

    wsa4000demo: fix for py2exe

--- examples/gui/setup.py
+++ examples/gui/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    console=['wsa4000.py'],
+    console=['wsa4000demo.py'],
     options={'py2exe':{
         'compressed':1,
         'bundle_files':1,
