78a94091b96e5272c1d08bc8ea06e38b4abb57ae
Ian Ward <ian@excess.org>
2012-12-21 18:44:24 -0500

    start development on next release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='thinkrf',
-    version='0.2.1',
+    version='0.3.0-dev',
     author='ThinkRF Corporation',
     author_email='support@thinkrf.com',
     packages=['thinkrf'],
