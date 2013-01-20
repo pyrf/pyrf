a7187700ce19cac349d233e15b340e794c2241a2
Ian Ward <ian@excess.org>
2013-01-20 16:04:21 -0500

    twsted_util: twisted version of read_data_and_reflevel

---
+++ pyrf/twisted_util.py
@@ -0,0 +1,25 @@
+from twisted.internet import defer
+
+@defer.inlineCallbacks
+def read_data_and_reflevel(dut, points=1024):
+    """
+    Wait for and capture a data packet and a reference level packet.
+
+    :returns: (data_pkt, reflevel_pkt)
+    """
+    # capture 1 packet
+    yield dut.capture(points, 1)
+
+    reference_pkt = None
+    # read until I get 1 data packet
+    while not dut.eof():
+        pkt = yield dut.read()
+
+        if pkt.is_data_packet():
+            break
+
+        if 'reflevel' in pkt.fields:
+            reference_pkt = pkt
+
+    yield pkt, reference_pkt
+
