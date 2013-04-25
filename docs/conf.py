ede9f5bda71c3b6932d2fe31111b759ece03a3ff
Ian Ward <ian@excess.org>
2013-04-25 15:43:10 -0400

    update version to 0.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012-2013, ThinkRF Corporation'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.5'
+version = '0.2.6'
 # The full version, including alpha/beta/rc tags.
 release = version # + '-dev'
 
