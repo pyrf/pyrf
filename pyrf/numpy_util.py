3d2a1f3c2efbf1cf3d2e071f4415947e4b03e075
Ian Ward <ian@excess.org>
2013-04-25 17:32:31 -0400

    fix to fft sample size fix

--- pyrf/numpy_util.py
+++ pyrf/numpy_util.py
@@ -51,7 +51,7 @@ def _compute_fft(i_data, q_data):
     windowed_iq = iq * numpy.hanning(len(i_data))
 
     power_spectrum = numpy.fft.fftshift(numpy.fft.fft(windowed_iq))
-    power_spectrum = 20 * numpy.log10(numpy.abs(power_spectrum)/len(fft_result))
+    power_spectrum = 20 * numpy.log10(numpy.abs(power_spectrum)/len(power_spectrum))
 
     median_index = len(power_spectrum) // 2
     power_spectrum[median_index] = (power_spectrum[median_index - 1]
@@ -64,7 +64,7 @@ def _compute_fft_i_only(i_data):
     windowed_i = i_data * numpy.hanning(len(i_data))
 
     power_spectrum = numpy.fft.fftshift(numpy.fft.fft(windowed_i))
-    power_spectrum = 20 * numpy.log10(numpy.abs(power_spectrum)/len(fft_result))
+    power_spectrum = 20 * numpy.log10(numpy.abs(power_spectrum)/len(power_spectrum))
 
     median_index = len(fft_result) // 2
     return fft_result[median_index+1:]
