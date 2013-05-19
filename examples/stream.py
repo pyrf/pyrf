62cfd39e64ade5a6e21631d0068c31d7b44bc4f6
Ian Ward <ian@excess.org>
2013-05-19 14:34:39 -0400

    stream.py: measure correct stream packets

--- examples/stream.py
+++ examples/stream.py
@@ -23,15 +23,19 @@ for spp in [min(2**i, 2**16-16) for i in range(9, 17)]:
     dut.spp(spp)
 
     runs = max(SAMPLES/spp, 1)
-    dut.stream_start()
+    dut.stream_start(spp)
+    start = None
+    while True:
+        pkt = dut.read()
+        if pkt.is_context_packet() and pkt.fields.get('streamid') == spp:
+            break
     start = time.time()
     for i in range(runs):
-        while not dut.eof():
+        while True:
             pkt = dut.read()
-
             if pkt.is_data_packet():
                 break
+    stop = time.time()
     dut.stream_stop()
     dut.flush()
-    stop = time.time()
     print 'spp %d: %f samples/second' % (spp, spp*runs/(stop-start))
