391e9c1bc2ce0674563c572235a7329d6995dad5
Ian Ward <ian@excess.org>
2013-05-18 20:42:58 -0400

    breaking backwards compat, so call this 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2012-2013, ThinkRF Corporation'
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.1'
+version = '0.4.0'
 # The full version, including alpha/beta/rc tags.
 release = version #+ '-dev'
 
