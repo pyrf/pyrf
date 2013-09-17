841cac6975acc1f537f1d092fdd9b402a1b0560f
Ian Ward <ian@excess.org>
2013-09-17 11:07:01 -0400

    discovery.py: use select instead of signal.alarm

--- examples/discovery.py
+++ examples/discovery.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python
 import socket
 import struct
-import signal
+import select
 
 SERVERPORT = 18331
 DISCOVERY_RESPONSE_FORMAT = '>LL20sLL'
-WAIT_TIME = 1
+WAIT_TIME = 0.125
 
 cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 cs.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+cs.setblocking(0)
 
 cs.sendto('Hello WSA', ('<broadcast>', SERVERPORT))
 
-signal.alarm(WAIT_TIME) # toy example, alarm() is not for real code
 while True:
+    ready, _, _ = select.select([cs], [], [], WAIT_TIME)
+    if not ready:
+        break
     data, (host, port) = cs.recvfrom(1024)
     resp = struct.unpack(DISCOVERY_RESPONSE_FORMAT, data)
     print "WSA4000" if resp[3] == 0 else "WSA5000",
