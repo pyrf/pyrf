0a16954800358edc501715f1a7cc1b09010a5ab6
Ian Ward <ian@excess.org>
2013-10-01 11:44:42 -0400

    update twisted discovery

--- examples/twisted_discovery.py
+++ examples/twisted_discovery.py
@@ -5,12 +5,12 @@ from twisted.internet import reactor
 import struct
 import platform
 
+from pyrf.devices.thinkrf import (DISCOVERY_UDP_PORT, DISCOVERY_QUERY,
+    parse_discovery_response)
+
 WAIT_TIME = 0.125
 
 class DiscoverWSAs(DatagramProtocol):
-    SERVERPORT = 18331
-    DISCOVERY_RESPONSE_FORMAT = '>LL20sLL'
-
     def startProtocol(self):
         import socket
         self.transport.socket.setsockopt(
@@ -21,13 +21,12 @@ class DiscoverWSAs(DatagramProtocol):
         else:
             destinations = ['<broadcast>']
         for d in destinations:
-            self.transport.socket.sendto("Hello WSA",
-                (d, self.SERVERPORT))
+            self.transport.socket.sendto(DISCOVERY_QUERY,
+                (d, DISCOVERY_UDP_PORT))
 
     def datagramReceived(self, data, (host, port)):
-        resp = struct.unpack(self.DISCOVERY_RESPONSE_FORMAT, data)
-        print "WSA4000" if resp[3] == 0 else "WSA5000",
-        print resp[2].split('\0',1)[0], "at", host
+        model, serial, firmware = parse_discovery_response(data)
+        print model, serial, firmware, 'at', host
 
 reactor.listenUDP(0, DiscoverWSAs())
 reactor.callLater(WAIT_TIME, reactor.stop)
