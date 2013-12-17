c6942ee885a993676023f7f82a7c3e161c5feee3
Ian Ward <ian@excess.org>
2013-12-17 14:49:26 -0500

    test sweep device: use actual RBW values

--- pyrf/tests/test_sweep_device.py
+++ pyrf/tests/test_sweep_device.py
@@ -42,7 +42,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxxx(64 bins)xxxxxxxxxxxxxxx]
         ^100M     ^110M     ^120M     ^130M
         """
-        self._plan42(100*M, 132*M, 32*M / 64,
+        self._plan42(100*M, 132*M, 500000,
             [(133*M, 32*M, 0, 1, 256, 62, 64, 0, 64)])
 
     def test_simple_single_just_inside(self):
@@ -50,7 +50,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxx(128 bins)xxxxxxxxxxxxxxx] <- 131.9M
         ^100M     ^110M     ^120M     ^130M
         """
-        self._plan42(100*M, 131.9*M, 31.9*M / 64,
+        self._plan42(100*M, 131.9*M, 496000,
             [(133*M, 32*M, 0, 1, 512, 124, 128, 0, 128)],
             fstop=132*M)
 
@@ -59,7 +59,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxx(64 bins)xxxxxxxxxxxxxxxx] <- 130.1M
         ^100M     ^110M     ^120M     ^130M
         """
-        self._plan42(100*M, 132.1*M, 32.1*M / 64,
+        self._plan42(100*M, 132.1*M, 501000,
             [(133*M, 32*M, 0, 1, 256, 62, 64, 0, 64)],
             fstop=132*M)
 
@@ -68,7 +68,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxxxx(64 bins)xxxxxxxxxxxxxx][xxxxxxx(64 bins)xxxxxxxxxxxxxx]
         ^100M     ^110M     ^120M     ^130M     ^140M     ^150M     ^160M
         """
-        self._plan42(100*M, 164*M, 64*M / 128,
+        self._plan42(100*M, 164*M, 500000,
             [(133*M, 32*M, 0, 1, 256, 62, 64, 0, 128)])
 
     def test_simple_double_points_up(self):
@@ -76,7 +76,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxxxx(128 bins)xxxxxxxxxxxxx][xxxxxxx(128 bins)xxxxxxxxxxxxx]
         ^100M     ^110M     ^120M     ^130M     ^140M     ^150M     ^160M
         """
-        self._plan42(100*M, 164*M, 64*M / 129,
+        self._plan42(100*M, 164*M, 496000,
             [(133*M, 32*M, 0, 1, 512, 124, 128, 0, 256)])
 
     def test_simple_double_points_half(self):
@@ -84,7 +84,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxxxx(32 bins)xxxxxxxxxxxxxx][xxxxxxxx(32 bins)xxxxxxxxxxxxx]
         ^100M     ^110M     ^120M     ^130M     ^140M     ^150M     ^160M
         """
-        self._plan42(100*M, 164*M, 64*M / 64,
+        self._plan42(100*M, 164*M, 1*M,
             [(133*M, 32*M, 0, 1, 128, 31, 32, 0, 64)])
 
     def test_simple_double_points_min(self):
@@ -92,7 +92,7 @@ class TestPlanSweep(unittest.TestCase):
         [xxxxxxx(32 bins)xxxxxxxxxxxxxx][xxxxxxxx(32 bins)xxxxxxxxxxxxx]
         ^100M     ^110M     ^120M     ^130M     ^140M     ^150M     ^160M
         """
-        self._plan42(100*M, 164*M, 64*M / 32,
+        self._plan42(100*M, 164*M, 2*M,
             [(133*M, 32*M, 0, 1, 128, 31, 32, 0, 64)])
 
     def test_simple_triple_exact(self):
@@ -100,7 +100,7 @@ class TestPlanSweep(unittest.TestCase):
         [x(64 bins)xxxx][x(64 bins)xxxx][x(64 bins)xxxx]
         ^100M     ^120M     ^140M     ^160M     ^180M
         """
-        self._plan42(100*M, 196*M, 96*M / 192,
+        self._plan42(100*M, 196*M, 500000,
             [(133*M, 32*M, 0, 1, 256, 62, 64, 0, 192)])
 
     def test_decimated_single_exact(self):
@@ -108,7 +108,7 @@ class TestPlanSweep(unittest.TestCase):
         [x(4096 bins)xx]
         ^100M           ^101M
         """
-        self._plan42(100*M, 101*M, 1*M / 4096.0,
+        self._plan42(100*M, 101*M, 244,
             [(133*M, 1*M, 32.5*M, 64, 8192, 2048, 4096, 0, 4096)])
 
     def test_decimated_double_exact(self):
@@ -116,7 +116,7 @@ class TestPlanSweep(unittest.TestCase):
         [x(4096 bins)xx][x(4096 bins)xx]
         ^100M           ^101M           ^102M
         """
-        self._plan42(100*M, 102*M, 2*M / 8192.0,
+        self._plan42(100*M, 102*M, 244,
             [(133*M, 1*M, 32.5*M, 64, 8192, 2048, 4096, 0, 8192)])
 
     def test_truncate_to_left_sweep(self):
@@ -124,7 +124,7 @@ class TestPlanSweep(unittest.TestCase):
         [x(32 bins)xxxx] ... [x(32 bins)xxxx]
         ^63M                                 ^1983M
         """
-        self._plan42(0, 2048*M, 2048*M / 200,
+        self._plan42(0, 2048*M, 1024000,
             [(96*M, 32*M, 0, 1, 128, 31, 32, 0, 1920)],
             fstart=63*M, fstop=1983*M)
 
