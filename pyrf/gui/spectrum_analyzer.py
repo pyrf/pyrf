321501806262bba0c0ca428ef51327553ac05c86
Ian Ward <ian@excess.org>
2014-01-21 17:16:42 -0500

    remove commented code

--- pyrf/gui/spectrum_analyzer.py
+++ pyrf/gui/spectrum_analyzer.py
@@ -24,8 +24,5 @@ def main():
     # late import because installReactor is being used
     from twisted.internet import reactor
     reactor.run()
-    # del sys.modules['twisted.internet.reactor'] 
-
-
 
 
