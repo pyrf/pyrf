8e1d009b85d88fbffc61162fa94480bb3727d3e2
Ian Ward <ian@excess.org>
2014-04-08 16:46:07 -0400

    gui_config whitespace

--- pyrf/gui/gui_config.py
+++ pyrf/gui/gui_config.py
@@ -34,12 +34,12 @@ class PlotState(object):
             'iq_output_path': 'DIGITIZER'}
         self.mhold = False
         self.mhold_fft = None
-        
+
         self.trig = False
         self.block_mode = True
         self.peak = False
- 
-        self.freq_range = None        
+
+        self.freq_range = None
         self.center_freq = center_freq
         self.bandwidth = device_properties.FULL_BW[self.dev_set['rfe_mode']]
         self.fstart = self.center_freq - self.bandwidth / 2
@@ -47,82 +47,82 @@ class PlotState(object):
         self.rbw = rbw
         self.enable_plot = True
         self.freq_sel = 'CENT'
-        
+
         self.ref_level = PLOT_YMAX
-        self.min_level = PLOT_YMIN 
+        self.min_level = PLOT_YMIN
         self.trig = False
         self.trig_set = TriggerSettings(TRIGGER_TYPE_NONE,
-                                        self.center_freq + 10e6, 
+                                        self.center_freq + 10e6,
                                         self.center_freq - 10e6,-100)
         self.device_properties = device_properties
-    
+
     def disable_block_mode(self, layout):
         self.block_mode = False
         util.enable_freq_cont(layout)
-        
+
     def enable_block_mode(self, layout):
         self.block_mode = True
         layout._bw_edit.setText('125.0')
         self.update_freq_set(bw = 125e6)
         layout._cfreq.click()
-        
+
         layout.update_freq()
         layout.update_freq_edit()
         util.disable_freq_cont(layout)
 
-    def disable_triggers(self, layout): 
+    def disable_triggers(self, layout):
         layout._plot.amptrig_line.setValue(-100)
         layout._plot.remove_trigger()
 
         self.trig_set = TriggerSettings(TRIGGER_TYPE_NONE,
-                                        self.center_freq + 10e6, 
-                                        self.center_freq - 10e6,-100) 
+                                        self.center_freq + 10e6,
+                                        self.center_freq - 10e6,-100)
         layout.update_trig()
         self.trig = False
     def enable_triggers(self, layout):
         self.trig = True
         self.trig_set = TriggerSettings(TRIGGER_TYPE_LEVEL,
-                                        self.center_freq + 10e6, 
+                                        self.center_freq + 10e6,
                                         self.center_freq - 10e6,-100)
-        layout._plot.add_trigger(self.trig_set.fstart, self.trig_set.fstop)                                 
-    
+        layout._plot.add_trigger(self.trig_set.fstart, self.trig_set.fstop)
+
     def update_freq_range(self, start, stop, size):
         self.freq_range = np.linspace(start, stop, size)
-        
+
 
 
     def update_freq_set(self,
-                          fstart=None, 
-                          fstop=None, 
-                          fcenter=None, 
-                          rbw=None, 
+                          fstart=None,
+                          fstop=None,
+                          fcenter=None,
+                          rbw=None,
                           bw=None):
         prop = self.device_properties
 
         rfe_mode = self.dev_set['rfe_mode']
         min_tunable = prop.MIN_TUNABLE[rfe_mode]
         max_tunable = prop.MAX_TUNABLE[rfe_mode]
-        
+
         if self.block_mode:
             decimation = self.dev_set['decimation']
         else:
             decimation = 1
         if fcenter is not None:
-            
+
             if self.block_mode:
-                self.bandwidth = prop.FULL_BW[rfe_mode] 
+                self.bandwidth = prop.FULL_BW[rfe_mode]
                 self.fstart = fcenter - ((self.bandwidth / 2)) / decimation
                 self.fstop =  fcenter + (self.bandwidth / 2) / decimation
             else:
-            
+
                 self.fstart = max(min_tunable, fcenter - (self.bandwidth / 2))
                 self.fstop = min(max_tunable, fcenter + (self.bandwidth / 2))
-            
-            self.bandwidth = (self.fstop - self.fstart) 
+
+            self.bandwidth = (self.fstop - self.fstart)
             self.center_freq = self.fstart + (self.bandwidth / 2)
             self.bin_size = max(1, int((self.bandwidth) / self.rbw))
             self.dev_set['freq'] =  fcenter
-            
+
         elif fstart is not None:
             fstart = min(fstart, self.fstop - prop.TUNING_RESOLUTION)
             self.fstart = fstart
