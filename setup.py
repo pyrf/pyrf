6e922ddecd68eada528275b3fa12256499627cd3
Ian Ward <ian@excess.org>
2014-04-29 15:53:00 -0400

    release 2.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='pyrf',
-    version='2.0.2-dev',
+    version='2.0.2',
     author='ThinkRF Corporation',
     author_email='support@thinkrf.com',
     packages=['pyrf', 'pyrf.devices', 'pyrf.connectors', 'pyrf.gui'],
