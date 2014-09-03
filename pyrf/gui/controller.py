23afbbe7d5725baf13fe44b4a689e11fd6955857
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2014-09-03 13:26:31 -0400

    moved rbw check whenever a state has been changed

--- pyrf/gui/controller.py
+++ pyrf/gui/controller.py
@@ -158,16 +158,10 @@ class SpecAController(QtCore.QObject):
         device_set.pop('iq_output_path')
         device_set.pop('trigger')
 
-        # check if RBW is appropriate for given mode
-        if self._state.rbw not in self._dut.properties.RBW_VALUES[self._state.rfe_mode()]:
-            rbw = self._dut.properties.RBW_VALUES[self._state.rfe_mode()][0]
-        else:
-            rbw = self._state.rbw
-
         self._sweep_device.capture_power_spectrum(
             self._state.center - self._state.span / 2.0,
             self._state.center + self._state.span / 2.0,
-            rbw,
+            self._state.rbw,
             device_set,
             mode=self._state.rfe_mode())
 
@@ -423,7 +417,15 @@ class SpecAController(QtCore.QObject):
             if not self._state or span != self._state.span:
                 changed.append('span')
 
+        if 'mode' in changed:
+            # check if RBW is appropriate for given mode
+            if state.rbw not in self._dut.properties.RBW_VALUES[state.rfe_mode()]:
+                if state.sweeping():
+                    state.rbw = self._dut.properties.RBW_VALUES[state.rfe_mode()][0]
+                else:
+                    state.rbw = self._dut.properties.RBW_VALUES[state.rfe_mode()][-1]
         self._state = state
+
         # start capture loop again when user switches output path
         # back to the internal digitizer XXX: very WSA5000-specific
         if 'device_settings.iq_output_path' in changed:
