d7f0d0ba1c098445f9d506bec43a7e8827c2d87c
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2014-09-09 10:22:27 -0400

    created new SpecAState when the center changed

--- pyrf/gui/controller.py
+++ pyrf/gui/controller.py
@@ -399,6 +399,7 @@ class SpecAController(QtCore.QObject):
 
         # make sure resolution of center are the same as the device's tunning resolution
         state.center = float(np.round(state.center, -1 * int(np.log10(self._dut.properties.TUNING_RESOLUTION))))
+        state = SpecAState(state, center=state.center)
 
         if not state.sweeping():
             # force span to correct value for the mode given
