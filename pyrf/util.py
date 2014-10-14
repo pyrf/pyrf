ed79595b72014118faab8866bdd4bac82be60f77
Ian Ward <ian@excess.org>
2014-10-14 15:29:39 -0400

    fix for SH/SHN decimated usable rev4+

--- pyrf/util.py
+++ pyrf/util.py
@@ -73,7 +73,7 @@ def compute_usable_bins(dut_prop, rfe_mode, points, decimation, fshift):
             start = 0
             usable_bins[i] = (start, run)
 
-    if dut_prop.DEFAULT_SAMPLE_TYPE.get(rfe_mode) == I_ONLY:
+    if decimation == 1 and dut_prop.DEFAULT_SAMPLE_TYPE.get(rfe_mode) == I_ONLY:
         # we're getting only 1/2 the bins
         usable_bins = [(x/2, y/2) for x, y in usable_bins]
 
