a01b86c5d16cf2863eb128f3243994955c0b3708
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2014-11-21 13:51:55 -0500

    added trigger controls to capture widget

--- pyrf/gui/capture_widget.py
+++ pyrf/gui/capture_widget.py
@@ -1,7 +1,8 @@
 from PySide import QtGui, QtCore
 from pyrf.gui.util import hide_layout
 from pyrf.gui.fonts import GROUP_BOX_FONT
-from pyrf.gui.widgets import QCheckBoxPlayback, QComboBoxPlayback
+from pyrf.gui.widgets import QCheckBoxPlayback, QComboBoxPlayback, QDoubleSpinBoxPlayback
+from pyrf.units import M
 
 # FIXME: move to device properties?
 MODE_TO_TEXT = {
@@ -46,9 +47,30 @@ class CaptureControls(QtGui.QWidget):
         self._single_button = QtGui.QPushButton('Single')
 
         self._mode_label = QtGui.QLabel('Mode:')
-        # self._mode_label.setMaximumWidth(40)
+
         self._mode = QComboBoxPlayback()
         self._mode.setToolTip("Change the device input mode")
+
+        self._level_trigger = QCheckBoxPlayback("Level Trigger")
+        self._level_trigger.setToolTip("Enable Frequency Level Triggers")
+
+        self._trig_fstart_label = QtGui.QLabel("Start:")
+        self._trig_fstart = QDoubleSpinBoxPlayback()
+
+        # FIXME: use values from device properties
+        self._trig_fstart.setRange(0, 20000)
+        self._trig_fstart.setSuffix(" MHz")
+
+        self._trig_fstop_label = QtGui.QLabel("Stop:")
+        self._trig_fstop = QDoubleSpinBoxPlayback()
+        # FIXME: use values from device properties
+        self._trig_fstop.setRange(0, 20000)
+        self._trig_fstop.setSuffix(" MHz")
+
+        self._trig_amp_label = QtGui.QLabel("Level:")
+        self._trig_amp = QDoubleSpinBoxPlayback()
+        self._trig_amp.setSuffix(" dBm")
+        self._trig_amp.setRange(-2000, 2000)
 
     def _build_layout(self, dut_prop=None):
 
@@ -59,6 +81,17 @@ class CaptureControls(QtGui.QWidget):
 
         grid.addWidget(self._conts_box, 0, 3, 1, 1)
         grid.addWidget(self._single_button, 0, 4, 1, 1)
+        grid.addWidget(self._level_trigger, 1, 0, 1, 2)
+
+        grid.addWidget(self._trig_fstart_label, 2, 0, 1, 1)
+        grid.addWidget(self._trig_fstart, 2, 1, 1, 1)
+
+        grid.addWidget(self._trig_fstop_label, 2, 3, 1, 1)
+        grid.addWidget(self._trig_fstop, 2, 4, 1, 1)
+
+        grid.addWidget(self._trig_amp_label, 1, 3, 1, 1)
+        grid.addWidget(self._trig_amp, 1, 4, 1, 1)
+        self._trig_state(False)
 
         self.setLayout(grid)
         self.resize_widget()
@@ -83,9 +116,38 @@ class CaptureControls(QtGui.QWidget):
             if self.gui_state.device_settings['iq_output_path'] == 'CONNECTOR':
                 self.controller.apply_device_settings(rfe_mode = input_mode)
 
+        def enable_trigger():
+            trigger_settings = self.gui_state.device_settings['trigger']
+            if self._level_trigger.isChecked():
+                self._trig_state(True)
+
+                start = self.gui_state.center - (self.gui_state.span / 4)
+                stop = self.gui_state.center + (self.gui_state.span / 4)
+                level = trigger_settings['amplitude']
+                self.controller.apply_device_settings(trigger = {'type': 'LEVEL',
+                                                                'fstart': start,
+                                                                'fstop': stop,
+                                                                'amplitude': trigger_settings['amplitude']})
+            else:
+                self.controller.apply_device_settings(trigger = {'type': 'NONE',
+                                                                'fstart': trigger_settings['fstart'],
+                                                                'fstop': trigger_settings['fstop'],
+                                                                'amplitude': trigger_settings['amplitude']})
+                self._trig_state(False)
+
+        def new_trigger():
+            self.controller.apply_device_settings(trigger = {'type': 'LEVEL',
+                                                    'fstart': self._trig_fstart.value() * M,
+                                                    'fstop': self._trig_fstop.value() * M,
+                                                    'amplitude': self._trig_amp.value()})
+
         self._mode.currentIndexChanged.connect(new_input_mode)
         self._single_button.clicked.connect(single_capture)
         self._conts_box.clicked.connect(cont_capture)
+        self._level_trigger.clicked.connect(enable_trigger)
+        self._trig_fstart.editingFinished.connect(new_trigger)
+        self._trig_fstop.editingFinished.connect(new_trigger)
+        self._trig_amp.editingFinished.connect(new_trigger)
 
     def device_changed(self, dut):
         self.dut_prop = dut.properties
@@ -94,16 +156,50 @@ class CaptureControls(QtGui.QWidget):
     def state_changed(self, state, changed):
         self.gui_state = state
         if 'playback' in changed:
+            # restore controls after playback is stopped
+            self._level_trigger.setEnabled(state.mode in self.dut_prop.LEVEL_TRIGGER_RFE_MODES)
             self._update_modes(current_mode=state.mode)
+            self._level_trigger.playback_value(False)
+
         if state.playback:
             # for playback simply update on every state change
             self._mode.playback_value(MODE_TO_TEXT[state.mode])
         
+        if 'device_settings.trigger' in changed:
+            if state.device_settings['trigger']['type'] == 'LEVEL':
+                trigger = state.device_settings['trigger']
+                self._trig_fstart.quiet_update(value=trigger['fstart'] / M)
+                self._trig_fstop.quiet_update(value=trigger['fstop'] / M)
+                self._trig_amp.quiet_update(value=trigger['amplitude'])
+            else:
+                if self._level_trigger.checkState():
+                    self._level_trigger.click()
+
         if 'device_settings.iq_output_path' in changed:
             if state.device_settings['iq_output_path'] == 'CONNECTOR':
                 self._update_modes(include_sweep=False)
+                self._level_trigger.setEnabled(False)
+                self._trig_fstart.setEnabled(False)
+                self._trig_fstop.setEnabled(False)
+                self._trig_amp.setEnabled(False)
+
             elif state.device_settings['iq_output_path'] == 'DIGITIZER':
                 self._update_modes()
+                self._trig_fstart.setEnabled(True)
+                self._trig_fstop.setEnabled(True)
+                self._trig_amp.setEnabled(True)
+                self._level_trigger.setEnabled(True)
+
+        if 'mode' in changed:
+            if state.mode not in self.dut_prop.LEVEL_TRIGGER_RFE_MODES:
+                # forcibly disable triggers
+                if self._level_trigger.isChecked():
+                    self._level_trigger.click()
+                self._trig_state(False)
+                self._level_trigger.setEnabled(False)
+
+            else:
+                self._level_trigger.setEnabled(True)
 
     def plot_changed(self, state, changed):
         if 'cont_cap_mode' in changed:
@@ -124,6 +220,12 @@ class CaptureControls(QtGui.QWidget):
 
         self._mode.quiet_update((MODE_TO_TEXT[m] for m in modes), current_mode)
         self._mode.setEnabled(True)
+
+    def _trig_state(self, state):
+        self._trig_fstart.setEnabled(state)
+        self._trig_amp.setEnabled(state)
+        self._trig_fstop.setEnabled(state)
+        self._trig = state
 
     def resize_widget(self):
         self.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
