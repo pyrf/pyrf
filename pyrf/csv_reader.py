6a7198ab64a465ded38114e77d1421317581707c
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2016-09-09 14:16:07 -0400

    added async callback call, fixed minor bugs in csv reader

--- pyrf/csv_reader.py
+++ pyrf/csv_reader.py
@@ -17,7 +17,10 @@ class CSVReader(object):
     :param filename: name of the file to be read
     :param callback: callback to use for async operation (not used if
                      file_name is using a :class:`PlainSocketConnector`)
+
     """
+    _file = None
+    device_id = ''
     def __init__(self, file_name, async_callback=None):
         self._file_name = file_name
         self.async_callback = async_callback
@@ -28,19 +31,22 @@ class CSVReader(object):
         # read first line, which is the comment line
         self._file_comment = self._file.readline()
         # read second line, which is the device ID line
-        self._device_id = self._file.readline()
-
+        self.device_id = self._file.readline()
         # read the header of the data format
         self._file.readline()
         self._line_num = 3
     
     def close_csv(self):
+        if self._file is not None:
             self._file.close()
-
+            self._file = None
     def read_data(self):
+        try:
+            data_header = self._file.readline().split(',')
+        except AttributeError:
+            return (0,0,[0,0])
 
-        data_header = self._file.readline().split(',')
-
+        # if we reached end of file, reset the file
         if data_header[0] == 'EOF':
             self._reset_file()
             data_header = self._file.readline().split(',')
@@ -56,6 +62,8 @@ class CSVReader(object):
         time_stamp = data_header[5]
         if self.async_callback is None:
             return (start, stop, pow_data)
+        else:
+            self.async_callback(start, stop, pow_data)
 
     def _reset_file(self):
         # reset the data file, and read first 3 lines
