165a4e4a8d5eb1fd67e28a123b366bd59fc5e2a2
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2017-02-15 15:00:02 -0500

    Fixed edge case of span less then usable BW

--- pyrf/sweep_device.py
+++ pyrf/sweep_device.py
@@ -50,6 +50,74 @@ class sweepSettings(object):
 
     # expected spectral points
     spectral_points = 0
+class SweepPlanner(object):
+    """
+    An object that plans a sweep based on  given paramaters.
+    """
+
+    def __init__(self, dev_prop):
+        self.dev_properties = dev_prop
+        self._prev_settings = sweepSettings()
+
+    def plan_sweep(self,fstart,fstop,rbw,mode,dev_settings = {}):
+        """
+        Plan the sweep given the inputs
+        """
+
+        #TODO CHECK FSTART, FSTOP, RBW, AND MODE, make sure they are valid
+        self.fstart = fstart
+        self.fstop = fstop
+        self.rbw = rbw
+        self.rfe_mode = mode
+
+        # initialize the sweep settings variable
+        sweep_settings = sweepSettings()
+
+        # assign the sweep mode
+        sweep_settings.rfe_mode = self.rfe_mode
+
+        if 'attneuator' in dev_settings:
+            sweep_settings.attenuation = dev_settings['attenuator']
+
+        # grab the usable bw of the current mode
+        self.usable_bw = self.dev_properties.USABLE_BW[self.rfe_mode]
+
+        # calculate the fstart of the sweep entry
+        sweep_settings.fstart = self.fstart + (self.usable_bw / 2)
+
+        # check if the calculated fstart is below the minimum tunable
+        if sweep_settings.fstart < self.dev_properties.MIN_TUNABLE[self.rfe_mode]:
+            sweep_settings.fstart = self.dev_properties.MIN_TUNABLE[self.rfe_mode]
+
+        # determine if DD mode is required
+        if self.fstart < self.dev_properties.MIN_TUNABLE[self.rfe_mode]:
+            sweep_settings.dd_mode = True
+            sweep_settings.fstart = self.dev_properties.MIN_TUNABLE[self.rfe_mode]
+
+        else:
+            sweep_settings.dd_mode = False
+
+        # grab the full banddwidth of the current mode
+        full_bw = self.dev_properties.FULL_BW[self.rfe_mode]
+
+        # assign the sweep entry's step frequency, take into account tuning resolution
+        sweep_settings.fstep = self.usable_bw - 100E3 #self.dev_properties.TUNING_RESOLUTION
+
+        # calculate the fstop of the sweep entry
+        required_steps = math.ceil((self.fstop - sweep_settings.fstart) / self.usable_bw)
+        sweep_settings.fstop = sweep_settings.fstart + (required_steps * self.usable_bw)
+
+        # calculate the required samples per packet based on the RBW
+        points = full_bw / self.rbw
+        sweep_settings.spp = int(32 * round(float(points)/32))
+
+        # calculate the expected number of spectral bins required for the SweepEntry
+        sweep_settings.spectral_points = int((self.fstop - self.fstart) / self.rbw)
+
+        self._prev_settings = sweep_settings
+
+        # return the sweep sweep_settings
+        return sweep_settings
 
 class SweepDevice(object):
     """
@@ -79,11 +147,12 @@ class SweepDevice(object):
     # keep track of the sweep settings
     _sweep_settings = None
 
-    # keep track of the packet count 
+    # keep track of the packet count
     packet_count = 0
 
-    # array to place spectral data 
+    # array to place spectral data
     spectral_data = []
+
     def __init__(self, real_device, async_callback=None):
 
         # initialize the real device
@@ -91,6 +160,9 @@ class SweepDevice(object):
 
         # keep track of the device properties
         self.dev_properties = self.real_device.properties
+
+        # initialize the sweep planner
+        self._sweep_planner = SweepPlanner(self.dev_properties)
 
         # create a sweep id
         self._sweep_id = random.randrange(0, 2**32-1) # don't want 2**32-1
@@ -141,82 +213,49 @@ class SweepDevice(object):
         :param min_points: smallest number of points per capture from real_device
         :type min_points: int
         """
+
         if continuous and not self.async_callback:
             raise SweepDeviceError(
                 "continuous mode only applies to async operation")
 
+        self.real_device.flush()
+        self.real_device.abort()
+        self.real_device.request_read_perm()
+
         # grab the device settings
         self.device_settings = device_settings
+        # keep track of the mode
+        self.rfe_mode = mode
+
+        # grab the usable bw of the current mode
+        self.usable_bw = self.dev_properties.USABLE_BW[self.rfe_mode]
 
         # keep track if this is a continoued swee
         self.continuous = continuous
-
-        # keep track of the mode
-        self.rfe_mode = mode
 
         # keep track of the fstart/fstop and rbw
         self.fstart = fstart
         self.fstop = fstop
         self.rbw = rbw
 
-        # plan the sweep
-        self._plan_sweep()
-
-        # capture the sweep data
-        return self._perform_full_sweep()
-
-    def _plan_sweep(self):
-
-        #TODO CHECK FSTART, FSTOP, RBW, AND MODE, make sure they are valid
-
-        # initialize the sweep settings variable
-        sweep_settings = sweepSettings()
-
-        # assign the sweep mode
-        sweep_settings.rfe_mode = self.rfe_mode
-
-        # if the sweep is continuous, do not re configure the device
-        if self.continuous:
-            return
-
-        # delete all sweep entries
+        # TODO Check if continuous is requested
         self.real_device.sweep_clear()
 
-        # determine if DD mode is required
-        if self.fstart < self.dev_properties.MIN_TUNABLE[self.rfe_mode]:
-            sweep_settings.dd_mode = True
-        else:
-            sweep_settings.dd_mode = False
+        # plan the sweep
+        self._sweep_settings = self._sweep_planner.plan_sweep(self.fstart,
+                                                              self.fstop,
+                                                              self.rbw,
+                                                              self.rfe_mode,
+                                                              self.device_settings)
 
-        # grab the usable bw of the current mode
-        self.usable_bw = self.dev_properties.USABLE_BW[self.rfe_mode]
-
-        # grab the full banddwidth of the current mode
-        full_bw = self.dev_properties.FULL_BW[self.rfe_mode]
-
-        # assign the sweep entry's step frequency
-        sweep_settings.fstep = usable_bw
-
-        # calculate the fstart of the sweep entry
-        sweep_settings.fstart = self.fstart - (usable_bw / 2)
-
-        # calculate the fstop of the sweep entry
-        required_steps = math.ceil((self.fstop - sweep_settings.fstart) / usable_bw)
-        sweep_settings.fstop = sweep_settings.fstart + (required_steps * usable_bw)
-
-        # calculate the required samples per packet based on the RBW
-        points = full_bw / self.rbw
-        sweep_settings.spp = int(32 * round(float(points)/32))
-
-        # calculate the expected number of spectral bins required for the SweepEntry
-        sweep_settings.spectral_points = (self.fstop - self.fstart) / self.rbw
-
-        self._sweep_settings = sweep_settings
         # configure the device with the sweep sweep_settings
         self.real_device.sweep_add(self._sweep_settings)
 
         # configure the iteration
         self.real_device.sweep_iterations(1)
+
+        # capture the sweep data
+        return self._perform_full_sweep()
 
     def _perform_full_sweep(self):
 
@@ -242,7 +281,7 @@ class SweepDevice(object):
 
         self._vrt_context = {}
         self.spectral_data = []
-
+        self.got_id = False
         # keep track of packets recieved
         self.packet_count = 0
 
@@ -254,6 +293,14 @@ class SweepDevice(object):
         # if the packet is a context packet
         if packet.is_context_packet():
             self._vrt_context.update(packet.fields)
+            return
+
+        # check to see if we recieved our sweep ID
+        if 'sweepid' in self._vrt_context:
+            self.got_id = True
+
+        # if no ID recieved, then continue
+        if not self.got_id:
             return
 
         # increment the packet count
@@ -275,15 +322,16 @@ class SweepDevice(object):
                 stop_bin = int(self.fstop / self.device_settings.FULL_BW['DD'])
 
                 # if there was only DD mode, append spectral data and send to client
-                self.spectral_data.append(pow_data[start_bin:stop_bin])
-                self.async_callback(self.fstart, self.fstop, self.spectral_data)
-            return
+                self.spectral_data = pow_data[start_bin:stop_bin]
+                self._emit_data()
 
-        
-        freq = self._vrt_context['rffreq']
+                self.spectral_data = pow_data[start_bin:stop_bin]
+                return
 
-        packet_start = freq - (self.usable_bw / 2)
-        packet_stop = freq - (self.usable_bw / 2)
+        packet_freq = self._vrt_context['rffreq']
+
+        packet_start = packet_freq - (self.usable_bw / 2)
+        packet_stop = packet_freq + (self.usable_bw / 2)
 
         # determine the usable bins in this config
         usable_bins = compute_usable_bins(self.dev_properties,
@@ -291,28 +339,43 @@ class SweepDevice(object):
                                           self._sweep_settings.spp,
                                           1,
                                           0)
+
+        usable_bins, start, stop = adjust_usable_fstart_fstop(self.dev_properties,
+                                                             self.rfe_mode,
+                                                             len(pow_data) * 2,
+                                                             1,
+                                                             packet_freq,
+                                                             packet.spec_inv,
+                                                             usable_bins)
+
         #trim the FFT data, note decimation is 1, fshift is 0
         trimmed_spectrum, edge_data, fstart, fstop = trim_to_usable_fstart_fstop(pow_data,
                                                                                  usable_bins,
                                                                                  packet_start,
                                                                                  packet_stop)
-        
-        if self._ss_index < len(self.plan):
+                                                    
+        # check if this is the last expected packet
+        if self.fstop <= packet_freq + (self.usable_bw / 2):
+
+            stop_bin = int(len(trimmed_spectrum) * ((packet_stop - self.fstop) /  (self.usable_bw)))
+            self.spectral_data = np.concatenate([self.spectral_data, trimmed_spectrum[:stop_bin]])
+            # send the data to the client
+            return self._emit_data()
+
+        else:
+            self.spectral_data = np.concatenate([self.spectral_data, trimmed_spectrum])
+
             return
 
-        self.bins = np.concatenate(self.bin_arrays)
+
+    def _emit_data(self):
+        # emit the data to the client
+
+        # if async callback is available, emit the data
         if self.async_callback:
             self.real_device.vrt_callback = None
-            self.saturated = self.got_saturation
-
-            self.async_callback(self.fstart, self.fstop, self.bins)
-            self.got_saturation = False
-            if self.continuous:
-                self._ss_index = 0
-                self._ss_received = 0
-                self.bins = []
+            self.async_callback(self.fstart, self.fstop, self.spectral_data)
             return
-        return (self.fstart, self.fstop, self.bins)
-
-
-
+        # return the values if using blocking sockets
+        else:
+            return (self.fstart, self.fstop, self.spectral_data)
